21fa04ed3a56da4e1b380872e88d36015bf84062
triciadang <c20tricia.dang@usafa.edu>
2019-04-25 08:47:27 -0700

    added comments

--- pex2.c
+++ pex2.c
@@ -17,26 +17,42 @@ int main(){
 
 	printf("%s", "Starting device test code...\n");
 
+	//opens the device, r/w access
 	fd = open("/dev/cs483driver", O_RDWR);
+
+	//error checking
 	if (fd<0){
 		perror("Failed to open the device...");
 		return errno;
 	}
 
+	//initializes str variable
 	char str[BUFFER_LENGTH] = "";
+	
+	//prompts user for input
 	printf("Type in a short string to send to the kernel module: ");
+
+	//gets string of 256 characters
 	fgets(str, BUFFER_LENGTH, stdin);
 
+	//writes input to device
 	printf("Writing to the device...\n");
 	ret = write(fd, str, BUFFER_LENGTH);
+
+	//error checking
 	if (ret < 0){
 		perror("Failed to write message to the device.");
 		return errno;
 	}
 
+	//initializes receive variable
 	char receive[BUFFER_LENGTH] = "";
 	printf("Reading from the device...\n");
+
+	//reads the response from the LKM
 	ret = read(fd, receive, BUFFER_LENGTH);
+
+	//error checking
 	if (ret < 0){
 		perror("Failed to read message from the device.");
 		return errno;
@@ -45,6 +61,8 @@ int main(){
 		printf("String read from the device: ");
 		printf("%s", receive);
 	}
+
+	//closes the device
 	close(fd);
 
 	return 0;
